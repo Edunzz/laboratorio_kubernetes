41584d3ea8396b63eb3ae4b35ecdf54157d1ac72
JoseRomero616 <jo.seeduardo25397stk@gmail.com>
2023-07-13 18:59:39 -0500

    Create app.js

---
+++ aks_cross/compra-app/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/compra/:id', async (req, res) => {
+    const { id } = req.params;
+    const response = await fetch(`http://fecha-app-service:3000/fecha/${id}`);
+    const data = await response.json();
+    const numeroCompra = Math.floor(Math.random() * 1000);
+    res.json({ ...data, numeroCompra });
+});
+
+app.listen(3000, () => console.log('Server started on port 3000'));
