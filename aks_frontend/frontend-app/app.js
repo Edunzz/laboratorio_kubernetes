a68aa6ca2fe64f99bbeca1cc5bfa0e646f666874
JoseRomero616 <jo.seeduardo25397stk@gmail.com>
2023-07-14 16:20:12 -0500

    Update app.js

--- aks_frontend/frontend-app/app.js
+++ aks_frontend/frontend-app/app.js
@@ -5,13 +5,13 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
-    const response = await fetch(`${process.env.BACKEND_IP}/`);
+    const response = await fetch(`http://${process.env.BACKEND_IP}/`);
     const productos = await response.json();
     res.render('index', { productos });
 });
 
 app.get('/compra/:id', async (req, res) => {
-    const response = await fetch(`${process.env.CROSS_IP}/compra/${id}`);
+    const response = await fetch(`http://${process.env.CROSS_IP}/compra/${id}`);
     const resultado = await response.text();
     res.render('compra', { resultado });
 });
