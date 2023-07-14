6e586be79d863af2bf06dd6aaa0a3b408ce0ebd2
JoseRomero616 <jo.seeduardo25397stk@gmail.com>
2023-07-14 16:54:45 -0500

    Update app.js

--- aks_cross/compra-app/app.js
+++ aks_cross/compra-app/app.js
@@ -10,8 +10,8 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/compra/:id', async (req, res) => {
     const { id } = req.params;
-    const response = await fetch(`http://fecha-app-service:4000/fecha/${id}`);
-    console.log(`http://fecha-app-service:4000/fecha/${id}`);
+    const response = await fetch(`http://fecha-app-service/fecha/${id}`);
+    console.log(`http://fecha-app-service/fecha/${id}`);
     const data = await response.json();
     const numeroCompra = Math.floor(Math.random() * 1000);
     res.json({ ...data, numeroCompra });
